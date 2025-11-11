bcbeef8e0320e57f60d65c1669107ef8a3d61702
evanswanjau <evanswanjau>
2025-11-11 19:34:23 +0300

    Implemented Core Skills Tasks
    1. Random list generator
    2. List comprehension below 10
    3. List comprehension below 10 using filter

--- core_skills.py
+++ core_skills.py
@@ -1,6 +1,15 @@
 import random
-# rand_list =
+# random list of 10 numbers between 1 and 20
+rand_list = random.sample(range(1, 20), 10)
 
-# list_comprehension_below_10 =
+# numbers below 10
+list_comprehension_below_10 = []
+for i in rand_list:
+  if i < 10:
+    list_comprehension_below_10.append(i)
 
-# list_comprehension_below_10 =
+# numbers below 10 using filter
+def belowTen(x):
+  return x < 10
+
+list_comprehension_below_10_filter = list(filter(belowTen, rand_list))
